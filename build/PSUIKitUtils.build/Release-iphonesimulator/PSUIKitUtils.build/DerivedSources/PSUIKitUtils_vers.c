6ef468aafffcd637f95ca0dbc3da7599eecc7684
philipp <philipp@schunker.at>
2019-12-01 10:33:49 +0100

    migrated to Swift 5

---
+++ build/PSUIKitUtils.build/Release-iphonesimulator/PSUIKitUtils.build/DerivedSources/PSUIKitUtils_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char PSUIKitUtilsVersionString[];
+ extern const double PSUIKitUtilsVersionNumber;
+
+ const unsigned char PSUIKitUtilsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:PSUIKitUtils  PROJECT:PSUIKitUtils-1" "\n";
+ const double PSUIKitUtilsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/PSUIKitUtils.build/Release-iphonesimulator/PSUIKitUtils.build/Objects-normal/x86_64/PSUIKitUtils_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/philipp/Documents/Projekte/PSUIKitUtils/build/PSUIKitUtils.build/Release-iphonesimulator/PSUIKitUtils.build/DerivedSources/PSUIKitUtils_vers.c
